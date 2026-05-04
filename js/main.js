801816cd006ba146f3f97a32701f5598b7762b6d
yg <dudrud0642@gmail.com>
2026-05-04 16:39:34 +0900

    포트폴리오 작업 완료

--- js/main.js
+++ js/main.js
@@ -16,3 +16,21 @@ navLinks.forEach((link) => {
     target.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 });
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 0) {
+    document.body.classList.add("scroll");
+  } else {
+    document.body.classList.remove("scroll");
+  }
+});
+
+window.addEventListener("load", function() {
+  setTimeout(() => {
+    const bars = document.querySelectorAll('.bar_in');
+    bars.forEach(bar => {
+      bar.classList.add('active');
+    });
+  }, 600);
+});
+
+
